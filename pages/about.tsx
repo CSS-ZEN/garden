1af1acd7fd160a8ce735894ef22f7f6bc0c05ce1
sy <somarl@live.com>
2021-04-11 20:09:57 +0800

    feat(page): init from csszengarden.com/217

--- pages/about.tsx
+++ pages/about.tsx
@@ -13,10 +13,10 @@ const About = () => (
             <title>About</title>
             <link rel="icon" href="/favicon.ico" />
         </Head>
-        <h1>👋 Hi, I’m <a href="https://github.com/csszen">@csszen</a></h1>
-        <p>Building <a href="https://github.com/csszen/garden">csszen/garden</a>, which originates from <a href="https://github.com/mezzoblue/csszengarden.com">mezzoblue/csszengarden.com</a>.</p>
+        <h1>👋 Hi, I’m <a href="https://github.com/csszen" target="_blank">@csszen</a></h1>
+        <p>Building <a href="https://github.com/csszen/garden" target="_blank">csszen/garden</a>, which originates from <a href="https://github.com/mezzoblue/csszengarden.com" target="_blank">mezzoblue/csszengarden.com</a>.</p>
         <p>Contributing welcomed!</p>
-        <p>You can find me at <a href="https://github.com/csszen">Github</a>.</p>
+        <p>You can find me at <a href="https://github.com/csszen" target="_blank">Github</a>.</p>
         <Button label="button" />
     </article>
 )
