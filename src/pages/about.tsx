c3b125a9788d7c69fcc863328b47c5586096bbab
sy <somarl@live.com>
2021-04-12 10:50:26 +0800

    chore(page): move page code to src

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -2,9 +2,9 @@
 import Head from 'next/head'
 
 import dynamic from 'next/dynamic'
-const Button = dynamic(() => import('../components/button'))
+const Button = dynamic(() => import('components/button'))
 
-import styles from '../styles/about.module.scss'
+import styles from 'styles/about.module.scss'
 
 
 const About = () => (
@@ -12,6 +12,9 @@ const About = () => (
         <Head>
             <title>About</title>
             <link rel="icon" href="/favicon.ico" />
+
+            <meta name="author" content="csszen" />
+            <meta name="description" content="Originate from csszengarden.com by mezzoblue." />
         </Head>
         <h1>👋 Hi, I’m <a href="https://github.com/csszen" target="_blank">@csszen</a></h1>
         <p>Building <a href="https://github.com/csszen/garden" target="_blank">csszen/garden</a>, which originates from <a href="https://github.com/mezzoblue/csszengarden.com" target="_blank">mezzoblue/csszengarden.com</a>.</p>
