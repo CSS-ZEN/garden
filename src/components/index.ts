e10cf315af1f77c2953a8288d34e8afbb9ffaefb
sy <somarl@live.com>
2021-04-14 11:29:38 +0800

    feat(component): add landing

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,4 +2,5 @@
 export { default as Button } from './button'
 export { default as Fabric } from './fabric'
 export { default as Head } from './head'
+export { default as Landing } from './landing'
 export { default as Quote } from './quote'
--- src/config.ts
+++ src/config.ts
@@ -2,5 +2,7 @@
 /** @description the default theme returned by /api/theme */
 export const DEFAULT_THEME_ID = 'f4b657c4e3b99c63281b079f66d4dc34'
 
-/** @description the default file returned when fetching theme gist */
+/** @description the default file returned when fetching theme gist by /api/theme/[gistid] */
 export const DEFAULT_THEME_FILE = 'theme.css'
+
+export const LANDING_THRESHOLD = 3500
