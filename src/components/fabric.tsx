c506b465657fe567bdf8246a9657be078b51bb88
sy <somarl@live.com>
2021-04-14 11:23:30 +0800

    chore(component): fabric support full

--- src/components/fabric.tsx
+++ src/components/fabric.tsx
@@ -2,22 +2,24 @@
 interface IProps {
     className?: string
     children: any
+    full?: boolean
     clearfix?: boolean
 }
 
 export default function Fabric ({
     className: className = '',
     children,
+    full = false,
     clearfix = false,
 }: IProps) {
     if (clearfix) return (
-        <div className={`fabric ${className}`}>
+        <div className={`fabric${full ? ' fabric--full' : ''} ${className}`}>
             {children}
         </div>
     )
 
     return (
-        <div className={`fabric ${className}`}>
+        <div className={`fabric${full ? ' fabric--full' : ''} ${className}`}>
             <div className="fabric-container">
                 {children}
             </div>
