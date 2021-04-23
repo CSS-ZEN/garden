50c9493a8badfec00e229c9426a59504fd30892e
sy <somarl@live.com>
2021-04-23 10:41:13 +0800

    fix(api): fix github rest rate limit

--- src/helpers/fetchGist.ts
+++ src/helpers/fetchGist.ts
@@ -1,7 +1,11 @@
 
 export default async function fetchGist (gistid: string) {
     const url = `https://api.github.com/gists/${gistid}`
-    const r = await fetch(url)
+    const r = await fetch(url, {
+        headers: {
+            Authorization: 'Basic ' + Buffer.from(`csszen:${process.env.GIST_TOKEN}`).toString('base64'),
+        },
+    })
     const {status, ok} = r
     const body = await r.json()
     return {ok, status, body}
