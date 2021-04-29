c2fd832b7260b59f2195ebf8a68ce949dd8f0d71
sy <somarl@live.com>
2021-04-29 17:06:05 +0800

    feat(page): monaco editor

---
+++ scripts/monaco.sh
@@ -0,0 +1,4 @@
+rm -rf public/monaco-editor
+mkdir public/monaco-editor
+cp -r node_modules/monaco-editor/min public/monaco-editor/min
+cp node_modules/monaco-editor/monaco.d.ts public/monaco-editor/monaco.d.ts
--- src/pages/submit/index.tsx
+++ src/pages/submit/index.tsx
@@ -1,16 +1,31 @@
 
+import {useEffect} from 'react'
 import {Head, Fabric, Button, Link} from 'src/components'
 import {useBroadcastChannel} from 'src/helpers'
 
 
 export default function Edit () {
-    const [n, setN] = useBroadcastChannel('test-channel', {n: 1})
+    const [n, setN] = useBroadcastChannel('test-channel', {n: 1, code: ''})
 
-    const handleClick = () => setN(p => ({n: p.n + 1}))
+    const handleClick = () => setN(p => ({n: p.n + 1, code: ''}))
+
+    useEffect(() => {
+        const {require} = window as ANY
+        require.config({paths: {vs: '/monaco-editor/min/vs'}})
+        require(['vs/editor/editor.main'], () => {
+            const container = document.getElementById('container')
+            if (container) monaco.editor.create(container, {
+                value: ['* {', '    box-sizing: border-box;', '}'].join('\n'),
+                language: 'css',
+            })
+        })
+    }, [])
 
     return (
         <Fabric>
-            <Head title="Edit | CSS Zen Garden" />
+            <Head title="Edit | CSS Zen Garden">
+                <script src="/monaco-editor/min/vs/loader.js" />
+            </Head>
 
             <Fabric clearfix>
                 Edit - {n.n}
@@ -18,6 +33,7 @@ export default function Edit () {
 
             <Fabric>
                 <Button onClick={handleClick} label="+" />
+                <div id="container" style={{width: '800px', height: '600px'}} />
             </Fabric>
 
             <Fabric>
