a0727c00183ccb750150ac0c790ee300e90a4ac5
sy <somarl@live.com>
2021-04-29 17:11:21 +0800

    chore(page): resolve monaco map

--- scripts/monaco.sh
+++ scripts/monaco.sh
@@ -1,4 +1,5 @@
 rm -rf public/monaco-editor
 mkdir public/monaco-editor
 cp -r node_modules/monaco-editor/min public/monaco-editor/min
+cp -r node_modules/monaco-editor/min-maps public/monaco-editor/min-maps
 cp node_modules/monaco-editor/monaco.d.ts public/monaco-editor/monaco.d.ts
