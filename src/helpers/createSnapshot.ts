361a87c2df04bafca568c6fb8654f163a91516c1
wrz <fw51640@gmail.com>
2021-05-12 13:58:36 +0800

    feat(api): add snapshot api

--- src/helpers/createSnapshot.ts
+++ src/helpers/createSnapshot.ts
@@ -9,7 +9,7 @@ export default async function createSnapshot ({gistid, mode='desktop'}:ISnapshot
     const username = process.env.SNAPSHOT_USERNAME
     const password = process.env.SNAPSHOT_PASSWORD
     const auth = 'Basic ' + Buffer.from(username + ':' + password).toString('base64')
-    await fetch(`${SNAPSHOT_SERVER}/api/snapshot/${mode}?url=https://czg.vercel.app/theme/${gistid}`, {
+    return await fetch(`${SNAPSHOT_SERVER}/api/snapshot/${mode}?url=https://czg.vercel.app/theme/${gistid}`, {
         headers: {Authorization: auth},
     })
 }
