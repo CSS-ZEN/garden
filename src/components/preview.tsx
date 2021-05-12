6c2039fe6e10bd2424b1f3790b6016bbf43cdf47
wrz <fw51640@gmail.com>
2021-05-12 14:03:23 +0800

    chore(component): catch img error to snapshot api

--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -1,6 +1,7 @@
-import styles from './preview.module.scss'
+import {useState} from 'react'
 import {AWSHOST} from 'src/config'
 import {IThemeManifest} from 'src/garden'
+import styles from './preview.module.scss'
 
 interface IPreviewProps {
     className?: string
@@ -10,14 +11,23 @@ interface IPreviewProps {
 
 
 export default function ThemePreview ({manifest, gistsid, className = ''}: IPreviewProps) {
-    const src = `https://${AWSHOST}/desktop/czg.vercel.app/theme/${gistsid}.jpg`
+    const [src, setSrc] = useState(`https://${AWSHOST}/desktop/czg.vercel.app/theme/${gistsid}.jpg`)
+    let errored = true
+    const onError = () => {
+        if (errored) {
+            setSrc(`_next/image?url=/api/snapshot/${gistsid}&w=1920&q=75`)
+            errored = true
+        }
+    }
+
     return (
         <div className={className}>
             <ul className={`${styles.gallery} ${styles['gallery-margin']}`}>
                 <li className={`${styles.thumb} ${styles['browser-frame']}`} id={`/theme/${gistsid}`}>
                     <a href={`/theme/${gistsid}`}>
                         <div className={styles['project-frame']}>
-                            <img src={src} alt="" />
+                            {/* <Image layout="fill" src={src} alt={src} /> */}
+                            <img onError={onError} src={src} alt={src} />
                         </div>
                     </a>
                     <p className={styles['thumb-caption']}>{manifest.name} By {manifest.author}</p>
